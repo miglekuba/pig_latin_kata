8947bd65e8105bf78eeb11175206e473e0a843f3
Migle Kubiliute <migle.kubiliute@gmail.com>
2019-10-08 15:07:52 +0100

    fist exercise

--- exercise_1.rb
+++ exercise_1.rb
@@ -10,12 +10,23 @@
 # Assume the words are lowercase.
 # Assume your user is a developer — e.g. you don't need to write a user interface.
 
-def pig_latin(word)
 
+def pig_latin(word)
+  consonant_letters = ["b", "c", "d", "f", "g", "h", "j", "k",
+     "l", "m", "n", "p", "q", "r", "s", "t", "v", "x", "z", "w", "y"]
+
+
+if consonant_letters.include?(word[0])
+  first = word[0]
+  word[0]= ""
+  word+first+"ay"
+else
+  word+"way"
+end
 end
 
 ## Tests:
-
+pig_latin("dog")
 require_relative './helpers/assert_equal'
 assert_equal(pig_latin('dog'), 'ogday')
 assert_equal(pig_latin('up'), 'upway')
